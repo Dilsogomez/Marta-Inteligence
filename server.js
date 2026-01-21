50080acbc264c6df0c79e83b55d4020653fd6c42
vandilsogomes.silva <vandilsogomes.silva@gmail.com>
2026-01-21 19:27:12 +0000

    fix: Listen on all network interfaces

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
